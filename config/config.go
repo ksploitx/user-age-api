1a46219e7ecfbc192bb0d4de2459c6c52fcebf69
Khushneet Singh <singhkhushneet601@gmail.com>
2026-06-18 23:32:25 +0530

    feat: add config loader, logger, and internal package scaffold

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	AppPort    string
+}
+
+func Load() *Config {
+	godotenv.Load()
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		AppPort:    os.Getenv("APP_PORT"),
+	}
+}
+
+func (c *Config) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName,
+	)
+}
