de134d93fd5101a2267f1f48041d76635a0714f8
Khushneet Singh <singhkhushneet601@gmail.com>
2026-06-18 23:24:33 +0530

    feat: add sqlc config and generated Go database code

---
+++ db/sqlc/query.sql
@@ -0,0 +1,22 @@
+-- name: CreateUser :one
+INSERT INTO users (name, dob)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET name = $1, dob = $2
+WHERE id = $3
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id;
